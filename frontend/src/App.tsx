6316ec76cb4f5cd661bdcfb822820a68756e65df
Ayushjo <ayushs24101@iiitnr.edu.in>
2026-06-30 15:51:54 +0530

    Update frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,30 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import NewTask from './pages/NewTask'
 import TaskView from './pages/TaskView'
+import History from './pages/History'
+import { AuthProvider } from './context/AuthContext'
+import { ToastProvider } from './components/ui'
+import RouteGuard from './components/RouteGuard'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/"          element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/tasks/new" element={<NewTask />} />
-        <Route path="/tasks/:id" element={<TaskView />} />
-        <Route path="*"          element={<Navigate to="/" replace />} />
-      </Routes>
+      <AuthProvider>
+        <ToastProvider>
+          <Routes>
+            <Route path="/"          element={<Home />} />
+            <Route path="/login"     element={<Login />} />
+            <Route path="/dashboard" element={<RouteGuard><Dashboard /></RouteGuard>} />
+            <Route path="/history"   element={<RouteGuard><History /></RouteGuard>} />
+            <Route path="/tasks/new" element={<RouteGuard><NewTask /></RouteGuard>} />
+            <Route path="/tasks/:id" element={<RouteGuard><TaskView /></RouteGuard>} />
+            <Route path="*"          element={<Navigate to="/" replace />} />
+          </Routes>
+        </ToastProvider>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
