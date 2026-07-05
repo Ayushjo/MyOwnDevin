8a83f4ee8c679bd04be4a84c5ea5a4f8cfbf01ce
Ayushjo <ayushs24101@iiitnr.edu.in>
2026-07-05 09:47:35 +0530

    Update frontend/vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
     },
   },
   server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3500',
